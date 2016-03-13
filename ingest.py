e948650daa99288c30b431132a4c9a9a37b4da3b
mrpsb <mrpsb@hotmail.com>
2016-03-13 00:05:18 +0000

    Comments in ingest.py and updated requirements

--- ingest.py
+++ ingest.py
@@ -40,31 +40,48 @@ def filter_tweet(tweet):
 
 if __name__=="__main__":
    
+    # Source text file
     file = TEXT_SOURCE
+
+    # Initialise list for tweets
     source_tweets=[]
+
+    # A lovely message for the console
     print ">>> Generating from {0}".format(file)
+
+    # create a list from the source file
     raw_tweets = list(open(file))
+
+    # decode each tweet from UTF-8, and filter it to kick out
+    # RT/MT, @, web links and the like
+    # This gives an array of plainish text tweets that are just
+    # sentences
+
     for twat in raw_tweets[:]:
         source_tweets.append(filter_tweet(twat.decode('UTF-8')))
         
-    # this section does the actual building of bot's brain
-    
     # something in Markov is broken so only order 2 works properly
     # Maybe one day I'll know enough to fix the bug
     # One day...
-        
+
+    # this section does the actual building of the bot's brain
+    # by cramming in cleaned up tweets one by one
+
+    # create Markov object
+
     mine = markov.MarkovChainer(2) # change "2" to "order" to use order from config file
 
-    ebook_tweet = ""  # this clears out any previous unsuccessful attempt
-
     for tweet in source_tweets:
+        # if the tweet has punctuation in it, then great
         if re.search('([\.\!\?\"\']$)', tweet):
             pass
+        # otherwise add a full stop at the end or it'll get upset
+        # when it tries to split the text
         else:
             tweet+="."
         mine.add_text(tweet)
 
-# Do something to stick object "mine" onto disk for later use
+    # Do something to stick objects "mine" and "source_tweets" onto disk for later use
 
     pickle.dump( mine , open("botbrain.p", "wb" ))
     pickle.dump( source_tweets, open("source_tweets.p","wb"))
