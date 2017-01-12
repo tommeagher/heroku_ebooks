91b8ef7d7e2ede264f7849ba612bd67c6dfb2bef
Tom Meagher <hello+git@tommeagher.com>
2017-01-12 08:51:26 -0500

    add testcorpus.txt for static testing example

--- local_settings_example.py
+++ local_settings_example.py
@@ -13,5 +13,5 @@ ODDS = 8 #How often do you want this to run? 1/8 times?
 ORDER = 2 #how closely do you want this to hew to sensical? 1 is low and 3 is high.
 DEBUG = True #Set this to False to start Tweeting live
 STATIC_TEST = False #Set this to True if you want to test Markov generation from a static file instead of the API.
-TEST_SOURCE = ".txt" #The name of a text file of a string-ified list for testing. To avoid unnecessarily hitting Twitter API.
+TEST_SOURCE = ".txt" #The name of a text file of a string-ified list for testing. To avoid unnecessarily hitting Twitter API. You can use the included testcorpus.txt, if needed.
 TWEET_ACCOUNT = "" #The name of the account you're tweeting to.
