9ae00d7389cdfbc1e3a9dcd571d3ef3b9f1db18c
Arron <runicsnow@live.com>
2017-04-17 13:35:08 +1000

    fixing range to match other documentation
    line 13 comments include range 1 - 3 but other places mention the range should be 2-4 due to errors from markov.py expecting minimum number that wont be reached if ORDER is not greater then 2
    
    updating the comments to match this

--- local_settings_example.py
+++ local_settings_example.py
@@ -10,7 +10,7 @@ MY_ACCESS_TOKEN_SECRET = 'Your Access Token Secret'
 
 SOURCE_ACCOUNTS = [""] #A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
 ODDS = 8 #How often do you want this to run? 1/8 times?
-ORDER = 2 #how closely do you want this to hew to sensical? 1 is low and 3 is high.
+ORDER = 2 #how closely do you want this to hew to sensical? 2 is low and 4 is high.
 DEBUG = True #Set this to False to start Tweeting live
 STATIC_TEST = False #Set this to True if you want to test Markov generation from a static file instead of the API.
 TEST_SOURCE = ".txt" #The name of a text file of a string-ified list for testing. To avoid unnecessarily hitting Twitter API. You can use the included testcorpus.txt, if needed.
