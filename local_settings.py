e371ea3fb9416648f9d4b9310e43e11e9ffd8320
Kale <34281910+KaleTheNoise@users.noreply.github.com>
2018-11-17 17:12:51 -0600

    Update local_settings.py
    Update to run more via Heroku config vars for easier setup and portability

--- local_settings.py
+++ local_settings.py
@@ -20,7 +20,7 @@ CLIENT_CRED_FILENAME = '' # the MASTODON client secret file you created for this
 USER_ACCESS_FILENAME = '' # The MASTODON user credential file you created at installation.
 
 # Sources (Twitter, Mastodon, local text file or a web page)
-TWITTER_SOURCE_ACCOUNTS = [""]  # A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
+TWITTER_SOURCE_ACCOUNTS = ["NONSTOPPUP"]  # A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
 MASTODON_SOURCE_ACCOUNTS = [""] # A list, e.g. ["@user@instance.tld"]
 SOURCE_EXCLUDE = r'^$'  # Source tweets that match this regexp will not be added to the Markov chain. You might want to filter out inappropriate words for example.
 STATIC_TEST = False  # Set this to True if you want to test Markov generation from a static file instead of the API.
@@ -30,11 +30,11 @@ SRC_URL = ['http://www.example.com/one', 'https://www.example.com/two']  # A com
 WEB_CONTEXT = ['span', 'h2']  # A comma-separated list of the tag or object to search for in each page above.
 WEB_ATTRIBUTES = [{'class': 'example-text'}, {}] # A list of dictionaries containing the attributes for each page.
 
-ODDS = 8  # How often do you want this to run? 1/8 times?
-ORDER = 2  # How closely do you want this to hew to sensical? 2 is low and 4 is high.
+ODDS = int(environ.get('TWEET_ODDS')) # How often do you want this to run? 1/8 times? Set in Heroku Config
+ORDER = int(environ.get('TWEET_ORDER'))  # How closely do you want this to hew to sensical? 2 is low and 4 is high.Set in Heroku Config
 
-DEBUG = True  # Set this to False to start Tweeting live
-TWEET_ACCOUNT = ""  # The name of the account you're tweeting to.
+DEBUG = False  # Set this to False to start Tweeting live
+TWEET_ACCOUNT = environ.get('TWEET_ACCOUNT') # The name of the account you're tweeting to. Set in Heroku Config
 
 #Configuration for Twitter parser. TEST_SOURCE will be re-used as as the corpus location.
 TWITTER_ARCHIVE_NAME = "tweets.csv" #Name of your twitter archive
