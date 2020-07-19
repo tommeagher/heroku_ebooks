666f753dad621eac8ba120b0be5a6bbef8542a07
Tom Meagher <hello+git@tommeagher.com>
2020-07-19 12:54:14 -0400

    Update local_settings.py
    Reverting changes to generic local_settings.py setup.

--- local_settings.py
+++ local_settings.py
@@ -14,14 +14,14 @@ MY_ACCESS_TOKEN_SECRET = environ.get('TWITTER_ACCESS_SECRET')#Your Access Token 
 
 # Configuration for Mastodon API
 ENABLE_MASTODON_SOURCES = False # Fetch mastodon statuses as a source?
-ENABLE_MASTODON_POSTING = True # Toot resulting status?
-MASTODON_API_BASE_URL = "https://botsin.space" # an instance url like https://botsin.space
-CLIENT_CRED_FILENAME = 'faebot_masto' # the MASTODON client secret file you created for this project
-USER_ACCESS_FILENAME = 'faebot' # The MASTODON user credential file you created at installation.
+ENABLE_MASTODON_POSTING = False # Toot resulting status?
+MASTODON_API_BASE_URL = "" # an instance url like https://botsin.space
+CLIENT_CRED_FILENAME = '' # the MASTODON client secret file you created for this project
+USER_ACCESS_FILENAME = '' # The MASTODON user credential file you created at installation.
 
 # Sources (Twitter, Mastodon, local text file or a web page)
-TWITTER_SOURCE_ACCOUNTS = ["transfaerie"]  # A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
-MASTODON_SOURCE_ACCOUNTS = ["OneiroFaerie@cybre.space"] # A list, e.g. ["@user@instance.tld"]
+TWITTER_SOURCE_ACCOUNTS = [""]  # A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
+MASTODON_SOURCE_ACCOUNTS = [""] # A list, e.g. ["@user@instance.tld"]
 SOURCE_EXCLUDE = r'^$'  # Source tweets that match this regexp will not be added to the Markov chain. You might want to filter out inappropriate words for example.
 STATIC_TEST = False  # Set this to True if you want to test Markov generation from a static file instead of the API.
 TEST_SOURCE = ".txt"  # The name of a text file of a string-ified list for testing. To avoid unnecessarily hitting Twitter API. You can use the included testcorpus.txt, if needed.
@@ -30,11 +30,11 @@ SRC_URL = ['http://www.example.com/one', 'https://www.example.com/two']  # A com
 WEB_CONTEXT = ['span', 'h2']  # A comma-separated list of the tag or object to search for in each page above.
 WEB_ATTRIBUTES = [{'class': 'example-text'}, {}] # A list of dictionaries containing the attributes for each page.
 
-ODDS = 0  # How often do you want this to run? 1/8 times?
-ORDER = 3  # How closely do you want this to hew to sensical? 2 is low and 4 is high.
+ODDS = 8  # How often do you want this to run? 1/8 times?
+ORDER = 2  # How closely do you want this to hew to sensical? 2 is low and 4 is high.
 
-DEBUG = False  # Set this to False to start Tweeting live
-TWEET_ACCOUNT = "faebot01"  # The name of the account you're tweeting to.
+DEBUG = True  # Set this to False to start Tweeting live
+TWEET_ACCOUNT = ""  # The name of the account you're tweeting to.
 
 #Configuration for Twitter parser. TEST_SOURCE will be re-used as as the corpus location.
 TWITTER_ARCHIVE_NAME = "tweets.csv" #Name of your twitter archive
